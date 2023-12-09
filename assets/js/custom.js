0b7077d47e5f1b22b1d030d4a9b4f517ce73cc84
Shreyas Nadkarni <65458352+Shreyas789@users.noreply.github.com>
2023-12-09 13:13:08 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,15 @@ function showElement(id) {
         x.style.display = "none";
     }
 }
+
+function toggleElement(id1,id2,clicked) {
+    var x = document.getElementById(id1);
+    var y = document.getElementById(id2);
+    if (clicked === id1){
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block;
+    }
+}
